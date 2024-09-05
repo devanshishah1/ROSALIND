579c3b3e79b65d0c1af389f701b2039a82e39635
devanshishah1 <158084071+devanshishah1@users.noreply.github.com>
2024-09-05 11:25:28 +0100

    next

---
+++ working_with_files.py
@@ -0,0 +1,28 @@
+Python 3.12.4 (v3.12.4:8e8a4baf65, Jun  6 2024, 17:33:18) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> with open('/Users/devanshishah/Downloads/rosalind_ini5 (2).txt', 'r') as f:
+...     lines = f.readlines()
+...     for i in range(1, len(lines), 2):  
+...         print(lines[i].strip())
+... 
+...         
+Some things in life are bad, they can really make you mad
+Other things just make you swear and curse
+When you're chewing on life's gristle, don't grumble give a whistle
+This will help things turn out for the best
+Always look on the bright side of life
+Always look on the right side of life
+If life seems jolly rotten, there's something you've forgotten
+And that's to laugh and smile and dance and sing
+When you're feeling in the dumps, don't be silly, chumps
+Just purse your lips and whistle, that's the thing
+So, always look on the bright side of death
+Just before you draw your terminal breath
+Life's a counterfeit and when you look at it
+Life's a laugh and death's the joke, it's true
+You see, it's all a show, keep them laughing as you go
+Just remember the last laugh is on you
+Always look on the bright side of life
+And always look on the right side of life
+Always look on the bright side of life
+And always look on the right side of life
